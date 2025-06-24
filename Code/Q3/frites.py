bce4880a12cae829c238de387f780a4bcf18f0cb
Dawnsawnn <michaud.samuel123@gmail.com>
2025-06-24 14:06:09 -0400

    Version complexe Q3

--- Code/Q3/frites.py
+++ Code/Q3/frites.py
@@ -2,6 +2,7 @@
 # Nom, Matricule
 
 import sys
+sys.setrecursionlimit(10**7)
 
 def read(input_file):
     file = open(input_file, "r")
@@ -16,34 +17,86 @@ def write(str_content, output_file):
     file.write(str_content)
     file.close()
 
-"""def placerFrites(n, pts, k):
-    orientation = [0] * n
-    
-    while True:
-        ok = True
-        for a in range(n):
-            x, y = pts[a]
-            r = orientation[a]
-            for b in range(a):
-                x2, y2 = pts[b]
-                r2 = orientation[b]
-                # conflit H–H sur même ligne
-                if r == r2 == 0 and y == y2 and abs(x - x2) <= 2*k:
-                    ok = False
-                    break
-                # conflit V–V sur même colonne
-                if r == r2 == 1 and x == x2 and abs(y - y2) <= 2*k:
-                    ok = False
-                    break
-            if not ok:
-                break
-        if ok:
-            return True
+def placerFrites(n, points, k):
 
-"""
+    N = n
+    size = 2 * N
+    g = [[] for _ in range(size)]
+    def add(u, v):
+        g[u].append(v)
+
+    # Conflits même ligne
+    rows = {}
+    for i, (x, y) in enumerate(points):
+        rows.setdefault(y, []).append((x, i))
+    for lst in rows.values():
+        lst.sort()
+        xs, idxs = zip(*lst)
+        l = 0
+        for r in range(len(xs)):
+            while xs[r] - xs[l] > 2 * k:
+                l += 1
+            for m in range(l, r):
+                i, j = idxs[m], idxs[r]
+                add(i, j + N)
+                add(j, i + N)
+
+    # Conflits même colonne
+    cols = {}
+    for i, (x, y) in enumerate(points):
+        cols.setdefault(x, []).append((y, i))
+    for lst in cols.values():
+        lst.sort()
+        ys, idxs = zip(*lst)
+        l = 0
+        for r in range(len(ys)):
+            while ys[r] - ys[l] > 2 * k:
+                l += 1
+            for m in range(l, r):
+                i, j = idxs[m], idxs[r]
+
+                add(i + N, j)
+                add(j + N, i)
+
+    visited = [False] * size
+    order = []
+    def dfs1(u):
+        visited[u] = True
+        for v in g[u]:
+            if not visited[v]:
+                dfs1(v)
+        order.append(u)
+
+    for u in range(size):
+        if not visited[u]:
+            dfs1(u)
+
+    # graphe inverse
+    gr = [[] for _ in range(size)]
+    for u in range(size):
+        for v in g[u]:
+            gr[v].append(u)
+
+    comp = [-1] * size
+    cid = 0
+    def dfs2(u):
+        comp[u] = cid
+        for v in gr[u]:
+            if comp[v] < 0:
+                dfs2(v)
+
+    for u in reversed(order):
+        if comp[u] < 0:
+            dfs2(u)
+            cid += 1
+
+    for i in range(N):
+        if comp[i] == comp[i + N]:
+            return False
+    return True
 
 
-    
+   
 
 def max_k(n, points):
     # Infini?
